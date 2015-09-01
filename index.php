43a1bdc3de1d0572c4480dd647860d4f99dcaa7a
Алексъ <alex13513@yandex.ru>
2015-09-01 21:41:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	include_once 'stat/start.php';
+?>
+
+
+<html>
+<head>
+</head>
+<body>
+
+
+Просто текст для проверки
+
+
+
+
+</body>
+</html>
+
+
+<?php
+	include_once $spath . '/stop.php';
+?>
